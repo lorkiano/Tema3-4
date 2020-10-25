a38b79aaf489c611f600756ff3dc5a8f89df2b87
fdurante <fdurante@alu.ucam.edu>
2020-10-25 10:48:30 +0100

    En desarrollo

---
+++ app/src/main/java/com/example/tema3_4/ActivitySaludo.java
@@ -0,0 +1,14 @@
+package com.example.tema3_4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ActivitySaludo extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo);
+    }
+}
--- app/src/main/java/com/example/tema3_4/MainActivity.java
+++ app/src/main/java/com/example/tema3_4/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.tema3_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Obtenemos referencias a los controles insertados en diseño
+    EditText nombre;
+    RadioGroup color;
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Capturamos
+        nombre = findViewById(R.id.editTextTextPersonName);
+        color = findViewById(R.id.rdColores);
+        boton = findViewById(R.id.btnEnviar);
+
+        // Listener para escuchar la acción del click del ratón
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Creamos Intent para pasar de actividad
+                Intent intent = new Intent(MainActivity.this, ActivitySaludo.class);
+
+                // Información a pasar entre actividades
+                Bundle bundle = new Bundle();
+                bundle.putAll(bundle);
+
+                // Pasamos la información al intent
+                intent.putExtras(bundle);
+
+                // Arrancamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+
     }
 }
